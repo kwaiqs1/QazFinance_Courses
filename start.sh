44743bdf78ad16b0bc3b6fed4b3ffd3c39f57bba
kwaiqs1 <nurken4ik2010@gmail.com>
2026-01-17 13:27:23 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,7 @@ set -e
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-gunicorn qazfinance_platform.wsgi:application --bind 0.0.0.0:${PORT}
+# создаём суперюзера (если включено через Variables)
+python manage.py ensure_superuser
+
+exec gunicorn qazfinance_platform.wsgi:application --bind 0.0.0.0:${PORT}
